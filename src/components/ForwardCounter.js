78a64d5c4fc05648da4e1fe8599aef0b03684527
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-09-22 19:30:27 -0400

    first commit

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,9 @@
+import useCounter from "../hooks/useCounter";
+import Card from "./Card";
+
+const ForwardCounter = () => {
+  const counter = useCounter();
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
